fa4007ec83947f680f67a19d53f5d372c6f129ac
y.drach <https://y-drach@bitbucket.org/y-drach/e-luscious.git>
2020-02-01 06:46:54 +0200

    carousel-2

--- script/main.js
+++ script/main.js
@@ -1,8 +1,6 @@
 ﻿
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
+
 
 var carouselElement = document.getElementsByClassName('product-slider');
 var productsSliderView = carouselElement[0].querySelector('.product-slider-view');
@@ -17,6 +15,13 @@ var shiftIndex = 0;
 
 var productsInSliderView;
 var productTileWidthInPercentage;
+
+var navigationItemsSelect = document.getElementById('navigation-items');
+var oneNavigationItemSelect = navigationItemsSelect.querySelectorAll('option');
+var menuItems = document.getElementsByClassName('navigation-items')[0];
+var oneMenuItem = menuItems.querySelectorAll('.navigation-item');
+var menuOpenTrigger;
+var selectedProduct;
 
 
 reset();
@@ -73,8 +78,45 @@ function validateSliding() {
 
 }
 
-function handleMenuChange() {
-    document.scrollIntoView({block: "center", behavior: "smooth"});
+function scrollToSliderAndSelectProduct() {
+    var destination;
+
+    selectedProduct = Number(navigationItemsSelect.value);
+    if (selectedProduct > 0) {
+        destination = $('.slider-section').offset().top;
+        $('html').animate({ scrollTop: destination }, 1000);
+        shiftIndex = 1-selectedProduct;
+    }
+    validateSliding();
+    hideAndShowArrows()
+}
+
+
+function closeMobileMenu() {
+    var menuOverlayToggle = document.getElementById('menu-overlay-toggle');
+    if (menuOverlayToggle.checked) {
+        menuOverlayToggle.checked = false;
+    }
+}
+
+
+var menuOpenTrigger = false;
+
+function triggerDesktopMenu() {
+    
+    if (menuOpenTrigger) {
+        menuItems.classList.remove('menu-active');
+        menuOpenTrigger = !menuOpenTrigger;
+    } else {
+        menuItems.classList.add('menu-active');
+        menuOpenTrigger = !menuOpenTrigger;
+    }
+    
+}
+
+function bindMenuItemsToSelect() {
+   
+    
 }
 
 
@@ -82,7 +124,11 @@ btnPrev.addEventListener('click', moveLeft);
 btnNext.addEventListener('click', moveRight);
 window.addEventListener('resize' , reset);
 window.addEventListener('orientationchange' , reset);
-
+navigationItemsSelect.addEventListener('change', closeMobileMenu);
+navigationItemsSelect.addEventListener('change', scrollToSliderAndSelectProduct);
+menuItems.addEventListener('click', closeMobileMenu);
+menuItems.addEventListener('click', triggerDesktopMenu);
+menuItems.addEventListener('click', bindMenuItemsToSelect);
 
 });
 
